0daecd2356242f60a93bc4e6718a5e8ef4b7efa4
Knut Ahlers <knut@ahlers.me>
2013-12-12 19:39:42 +0100

    Initial version of trac testing VM

---
+++ README.md
@@ -0,0 +1,9 @@
+# luzifer / tractest
+
+## Installation
+
+    git clone git://github.com/Luzifer/tractest.git
+    cd tractest
+    vagrant up
+
+This is everything. Vagrant will boot up an amd64 Ubuntu server with Trac=0.11 and you can access [http://localhost:8000/](http://localhost:8000/) after provisioning is finished successfully.
---
+++ modules/trac/manifests/init.pp
@@ -0,0 +1,50 @@
+class trac {
+
+  package { 'python-setuptools': }
+  package { 'build-essential': }
+  package { 'python-dev': }
+  package { 'python-pip':
+    require => [ Package['build-essential'], Package['python-dev'] ]
+  }
+
+  package { 'setuptools':
+    provider => 'pip',
+    require => Package['python-pip']
+  }
+
+  package { 'libsqlite3-dev': }
+  package { 'pysqlite':
+    provider => 'pip',
+    require => [ Package['python-pip'], Package['setuptools'], Package['libsqlite3-dev'] ]
+  }
+
+  package { 'Genshi':
+    ensure => '0.6',
+    provider => 'pip',
+    require => [ Package['python-pip'], Package['setuptools'] ]
+  }
+
+  package { 'trac':
+    provider => 'pip',
+    ensure => '0.11',
+    require => [ Package['python-pip'], Package['setuptools'], Package['Genshi'] ]
+  }
+
+  exec { 'create_trav_env':
+    command => "/usr/local/bin/trac-admin trac initenv 'My Project' 'sqlite:db/trac.db' 'svn' '/home/vagrant/repos'",
+    unless => '/usr/bin/test -d /home/vagrant/trac',
+    user => 'vagrant',
+    require => Package['trac']
+  }
+
+  file { '/etc/init/trac.conf':
+    source => 'puppet:///modules/trac/upstart_trac.conf'
+  }
+
+  service { 'trac':
+    ensure => running,
+    provider => 'upstart',
+    require => [ Package['trac'], File['/etc/init/trac.conf'] ]
+  }
+
+}
